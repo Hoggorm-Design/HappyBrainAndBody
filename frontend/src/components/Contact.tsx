16b8c4b2a631589f46ad26cc914124a68233a754
August Solli Middelkoop <august.kampen@gmail.com>
2024-12-28 20:52:08 +0100

    docs: added documentation for all sanity components.

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,4 +1,4 @@
-import { FiPhone, FiExternalLink } from 'react-icons/fi';
+import { FiPhone } from 'react-icons/fi';
 import useContact from '../hooks/useContact';
 
 const Contact = () => {
@@ -8,24 +8,26 @@ const Contact = () => {
     return (
         <section className="flex flex-col lg:flex-row justify-between p-[50px] 2xl:px-[100px] h-auto gap-14">
             {/* Contact Information - Left */}
-            <div className="flex-1">
+            <article className="flex-1">
                 <h2 id="kontakt" className="header font-semibold mb-8 text-2xl">
                     {contactData?.header || 'Contact'}
                 </h2>
-
-                <div className="flex flex-col gap-8">
+                <h3 className="font-semibold">Nettsider</h3>
+                <br/>
+                <div className="flex flex-col gap-4">
                     <div className="flex items-start">
-                        <FiExternalLink className="mr-4 mt-0.5 sub-header" />
                         <p>{contactData?.website1link}</p>
                     </div>
                     {contactData?.website2link && (
                         <div className="flex items-start">
-                            <FiExternalLink className="mr-4 mt-0.5 sub-header" />
                             <p>{contactData.website2link}</p>
                         </div>
                     )}
+                    <h3 className="font-semibold">Telefon og mail</h3>
+                    <br/>
                     <div className="flex items-start">
-                        <FiPhone className="mr-4 mt-0.5 sub-header" />
+                        <FiPhone className="mr-4 mt-0 sub-header"/>
+
                         <a
                             href={`tel:${contactData?.phonenumber}`}
                             className="underline text"
@@ -33,11 +35,19 @@ const Contact = () => {
                             {contactData?.phonenumber}
                         </a>
                     </div>
-                </div>
-            </div>
+                    <div className="flex items-start">
+                        <a
+                            href={`#`}
+                            className="underline text"
+                        >
+                            midlertidig uten mail
+                        </a>
+                    </div>
+                    </div>
+            </article>
 
             {/* Google Maps - Right */}
-            <div className="flex-1">
+            <article className="flex-1">
                 <iframe
                     className="w-full h-[500px] lg:h-[500px] rounded-lg shadow-lg p-4 bg-white"
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2077.833107439263!2d10.758490393805086!3d59.96173539559648!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x464171e334986e47%3A0xd49a34f3aeb5e59!2sKorsvoll%20Terrasse%201B%2C%200881%20Oslo%2C%20Norway!5e1!3m2!1sen!2sus!4v1735414657658!5m2!1sen!2sus"
@@ -45,7 +55,7 @@ const Contact = () => {
                     referrerPolicy="no-referrer-when-downgrade">
 
                 </iframe>
-            </div>
+            </article>
         </section>
     );
 };
