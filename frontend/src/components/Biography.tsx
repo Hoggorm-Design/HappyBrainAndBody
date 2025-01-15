61ea6929fdf0ffb22af0d7c306377ebc740c3063
August Solli Middelkoop <august.kampen@gmail.com>
2025-01-15 23:29:08 +0100

    Multiple stuff

--- frontend/src/components/Biography.tsx
+++ frontend/src/components/Biography.tsx
@@ -17,26 +17,31 @@ export default function Biography() {
         return <p>Ingen biografi tilgjengelig</p>;
     }
     return (
-        <section id="Var-lege" className="flex flex-col 2xl:flex-row gap-5 items-center justify-around bg-white h-full w-full p-[30px] 2xl:px-[100px] py-36">
-            <div className="w-full 2xl:w-auto flex justify-start items-center">
+        <section
+            id="Var-lege"
+            className="flex flex-col 2xl:flex-row gap-5 items-start justify-around bg-white h-full w-full p-[30px] 2xl:px-[100px] py-36"
+        >
+            {/* Image Section */}
+            <div className="w-full 2xl:w-auto flex justify-start items-start">
                 <img
                     src={post.mainImage?.asset?.url}
                     alt={post.alt}
                     className="max-h-[400px] max-w-[400px] w-auto h-auto object-contain"
                 />
             </div>
-            <div className="flex flex-col justify-center gap-5 h-3/5 xl:h-full">
+            <div className="flex flex-col justify-start gap-5 h-auto">
                 <div>
-                    <div>
-                        <h2 className="header font-semibold">{post.title}</h2>
-                        <h3 className="sub-header">{post.profession}</h3>
-                    </div>
-                    <div className="mt-5 text">
-                        <p className="block mt-4 text-left overflow-hidden text-ellipsis break-words">{post.body}</p>
+                    <h2 className="header font-semibold">{post.title}</h2>
+                    <h3 className="sub-header">{post.profession}</h3>
+                </div>
 
-                    </div>
+                <div className="mt-5 text">
+                    <p className="block mt-4 text-left overflow-hidden text-ellipsis break-words">
+                        {post.body}
+                    </p>
                 </div>
             </div>
         </section>
+
     );
 }
